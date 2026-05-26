b0e9364f8e3ec762e1911e5320f1e72d2283a87b
haresaleem12-glitch <haresaleem12@gmail.com>
2026-05-26 23:34:43 +0500

    Update reddit_discord_bot.py

--- reddit_discord_bot.py
+++ reddit_discord_bot.py
@@ -14,7 +14,7 @@ DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 CHANNEL_ID    = int(os.getenv("CHANNEL_ID", "0"))
 
 SUBREDDITS = ["long_porn", "Porn", "boobs"]
-INTERVAL_MINUTES = 1
+INTERVAL_MINUTES = 20
 POSTS_PER_RUN = 2
 SEEN_FILE = "seen_posts.json"
 MAX_FILE_SIZE_MB = 24
